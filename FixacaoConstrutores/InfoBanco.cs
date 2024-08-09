d7fab5b282a6273d24d74bac502751d1ab8db3e2
Igor Leite <igorlfilgueiras@gmail.com>
2024-08-08 23:20:35 -0300

    Banco

---
+++ FixacaoConstrutores/InfoBanco.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace FixacaoConstrutores
+{
+    internal class InfoBanco
+    {
+        public int IdConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public InfoBanco(int idconta, string nome)
+        {
+            IdConta = idconta;
+            Nome = nome;
+            Saldo = 0;
+        }
+
+        public InfoBanco(int idconta, string nome, double saldo) : this(idconta, nome)
+        {
+            Saldo = saldo;
+        }
+        
+        public void AdicionarSaldo(double depos) 
+        {
+             Saldo += depos;
+        }
+
+        public void RetirarSaque(double saque)
+        { 
+             Saldo -= saque;
+            Saldo -= 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+            + IdConta
+            + ", Titular: "
+            + Nome
+            + ", Saldo: $"
+            + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ FixacaoConstrutores/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Channels;
+using System.Globalization;
+
+namespace FixacaoConstrutores 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            int nconta;
+            string cnome;
+            string verif;
+            InfoBanco i;
+
+            Console.Write("Digite o número da conta: ");
+            nconta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular: ");
+            cnome = Console.ReadLine();
+            Console.Write("Haverá um depósito inicial (Sim/Nao)? ");
+            verif = Console.ReadLine();
+            if (verif == "Sim")
+            {
+                Console.WriteLine();
+                Console.Write("Digite o valor do depósito inicial: ");
+                double inidepos = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                i = new InfoBanco(nconta, cnome, inidepos);
+                Console.WriteLine("Dados da conta: " + i, CultureInfo.InvariantCulture);
+            }
+            else {
+                Console.WriteLine();
+                i = new InfoBanco(nconta, cnome);
+                Console.WriteLine("Dados da conta: " + i, CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine();
+            Console.Write("Digite um valor para Depósito: ");
+            double depos = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            i.AdicionarSaldo(depos);            
+            Console.WriteLine("Dados da conta atualizados: " + i, CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            i.RetirarSaque(saque);
+            Console.WriteLine("Dados da conta atualizados: " + i, CultureInfo.InvariantCulture);
+        }
+    }
+}
